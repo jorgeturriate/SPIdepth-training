19299f7b04eb4d5b6def0d976b13a7bb5129e87d
jorgeturriate <jorge_xt@hotmail.com>
2025-04-08 12:31:37 +0200

    Kitti dataset class names changed to avoid duplicate names with kitti_dataset_cloud file

--- datasets/kitti_dataset.py
+++ datasets/kitti_dataset.py
@@ -10,16 +10,45 @@ import os
 import skimage.transform
 import numpy as np
 import PIL.Image as pil
+import gcsfs
 
 from kitti_utils import generate_depth_map
 from .mono_dataset import MonoDataset
 
+class GCSHandler:
+    def __init__(self, data_path):
+        self.is_gcs = data_path.startswith("gs://")
+        if self.is_gcs:
+            self.fs = gcsfs.GCSFileSystem()
+            self.data_path = data_path.replace("gs://", "")
+        else:
+            self.fs = None
+            self.data_path = data_path
 
-class KITTIDatasetBACK(MonoDataset):
+    def open_image(self, path):
+        if self.is_gcs:
+            with self.fs.open(os.path.join(self.data_path, path), 'rb') as f:
+                return pil.open(f).convert('RGB')
+        else:
+            return pil.open(os.path.join(self.data_path, path)).convert('RGB')
+
+    def open_depth(self, path):
+        if self.is_gcs:
+            with self.fs.open(os.path.join(self.data_path, path), 'rb') as f:
+                return pil.open(f)
+        else:
+            return pil.open(os.path.join(self.data_path, path))
+
+    def get_file_path(self, *args):
+        return os.path.join(*args)
+
+
+
+class KITTIDataset(MonoDataset):
     """Superclass for different types of KITTI dataset loaders
     """
     def __init__(self, *args, **kwargs):
-        super(KITTIDatasetBACK, self).__init__(*args, **kwargs)
+        super(KITTIDataset, self).__init__(*args, **kwargs)
 
         # NOTE: Make sure your intrinsics matrix is *normalized* by the original image size.
         # To normalize you need to scale the first row by 1 / image_width and the second row
@@ -33,49 +62,48 @@ class KITTIDatasetBACK(MonoDataset):
 # K是相机内参 stereo_T是相机外参，也就是转换矩阵,4行4列，16维度
         self.full_res_shape = (1242, 375)
         self.side_map = {"2": 2, "3": 3, "l": 2, "r": 3}
+        self.gcs = GCSHandler(self.data_path)
 
     def check_depth(self):
         line = self.filenames[0].split()
         scene_name = line[0]
         frame_index = int(line[1])
 
-        velo_filename = os.path.join(
-            self.data_path,
+        velo_rel_path = os.path.join(
             scene_name,
             "velodyne_points/data/{:010d}.bin".format(int(frame_index)))
 
-        return os.path.isfile(velo_filename)
+        if self.gcs.is_gcs:
+            gcs_full_path = os.path.join(self.gcs.data_path, velo_rel_path)
+            return self.gcs.fs.exists(gcs_full_path)
+        else:
+            local_path = os.path.join(self.data_path, velo_rel_path)
+            return os.path.isfile(local_path)
 
     def get_color(self, folder, frame_index, side, do_flip):
-        color = self.loader(self.get_image_path(folder, frame_index, side))
-
+        path = self.get_image_path(folder, frame_index, side)
+        color = self.gcs.open_image(path)
         if do_flip:
             color = color.transpose(pil.FLIP_LEFT_RIGHT)
-
         return color
 
 
-class KITTIRAWDatasetBACK(KITTIDataset):
+class KITTIRAWDataset(KITTIDataset):
     """KITTI dataset which loads the original velodyne depth maps for ground truth
     """
     def __init__(self, *args, **kwargs):
-        super(KITTIRAWDatasetBACK, self).__init__(*args, **kwargs)
+        super(KITTIRAWDataset, self).__init__(*args, **kwargs)
 
     def get_image_path(self, folder, frame_index, side):
         f_str = "{:010d}{}".format(frame_index, self.img_ext)
-        image_path = os.path.join(
-            self.data_path, folder, "image_0{}/data".format(self.side_map[side]), f_str)
-        return image_path
+        return self.gcs.get_file_path(folder, "image_0{}/data".format(self.side_map[side]), f_str)
 
     def get_depth(self, folder, frame_index, side, do_flip):
-        calib_path = os.path.join(self.data_path, folder.split("/")[0])
+        calib_path = self.gcs.get_file_path(folder.split("/")[0])
 
-        velo_filename = os.path.join(
-            self.data_path,
-            folder,
-            "velodyne_points/data/{:010d}.bin".format(int(frame_index)))
+        velo_path = self.gcs.get_file_path(folder, "velodyne_points/data", "{:010d}.bin".format(frame_index))
 
-        depth_gt = generate_depth_map(calib_path, velo_filename, self.side_map[side])
+        depth_gt = generate_depth_map(calib_path, velo_path, self.side_map[side], use_gcs=True, gcs=self.gcs.fs, gcs_root=self.gcs.data_path)
         depth_gt = skimage.transform.resize(
             depth_gt, self.full_res_shape[::-1], order=0, preserve_range=True, mode='constant')
 
@@ -85,46 +113,32 @@ class KITTIRAWDatasetBACK(KITTIDataset):
         return depth_gt
 
 
-class KITTIOdomDatasetBACK(KITTIDataset):
+class KITTIOdomDataset(KITTIDataset):
     """KITTI dataset for odometry training and testing
     """
     def __init__(self, *args, **kwargs):
-        super(KITTIOdomDatasetBACK, self).__init__(*args, **kwargs)
+        super(KITTIOdomDataset, self).__init__(*args, **kwargs)
 
     def get_image_path(self, folder, frame_index, side):
         f_str = "{:06d}{}".format(frame_index, self.img_ext)
-        image_path = os.path.join(
-            self.data_path,
-            "sequences/{:02d}".format(int(folder)),
-            "image_{}".format(self.side_map[side]),
-            f_str)
-        return image_path
+        return self.gcs.get_file_path("sequences/{:02d}".format(int(folder)), "image_{}".format(self.side_map[side]), f_str)
 
 
-class KITTIDepthDatasetBACK(KITTIDataset):
+class KITTIDepthDataset(KITTIDataset):
     """KITTI dataset which uses the updated ground truth depth maps
     """
     def __init__(self, *args, **kwargs):
-        super(KITTIDepthDatasetBACK, self).__init__(*args, **kwargs)
+        super(KITTIDepthDataset, self).__init__(*args, **kwargs)
 
     def get_image_path(self, folder, frame_index, side):
         f_str = "{:010d}{}".format(frame_index, self.img_ext)
-        image_path = os.path.join(
-            self.data_path,
-            folder,
-            "image_0{}/data".format(self.side_map[side]),
-            f_str)
-        return image_path
+        return self.gcs.get_file_path(folder, "image_0{}/data".format(self.side_map[side]), f_str)
 
     def get_depth(self, folder, frame_index, side, do_flip):
         f_str = "{:010d}.png".format(frame_index)
-        depth_path = os.path.join(
-            self.data_path,
-            folder,
-            "proj_depth/groundtruth/image_0{}".format(self.side_map[side]),
-            f_str)
+        depth_path = self.gcs.get_file_path(folder, "proj_depth/groundtruth/image_0{}".format(self.side_map[side]), f_str)
 
-        depth_gt = pil.open(depth_path)
+        depth_gt = self.gcs.open_depth(depth_path)
         depth_gt = depth_gt.resize(self.full_res_shape, pil.NEAREST)
         depth_gt = np.array(depth_gt).astype(np.float32) / 256
 
--- datasets/kitti_dataset_BACK.py
+++ datasets/kitti_dataset_BACK.py
@@ -10,45 +10,16 @@ import os
 import skimage.transform
 import numpy as np
 import PIL.Image as pil
-import gcsfs
 
 from kitti_utils import generate_depth_map
 from .mono_dataset import MonoDataset
 
-class GCSHandler:
-    def __init__(self, data_path):
-        self.is_gcs = data_path.startswith("gs://")
-        if self.is_gcs:
-            self.fs = gcsfs.GCSFileSystem()
-            self.data_path = data_path.replace("gs://", "")
-        else:
-            self.fs = None
-            self.data_path = data_path
 
-    def open_image(self, path):
-        if self.is_gcs:
-            with self.fs.open(os.path.join(self.data_path, path), 'rb') as f:
-                return pil.open(f).convert('RGB')
-        else:
-            return pil.open(os.path.join(self.data_path, path)).convert('RGB')
-
-    def open_depth(self, path):
-        if self.is_gcs:
-            with self.fs.open(os.path.join(self.data_path, path), 'rb') as f:
-                return pil.open(f)
-        else:
-            return pil.open(os.path.join(self.data_path, path))
-
-    def get_file_path(self, *args):
-        return os.path.join(*args)
-
-
-
-class KITTIDataset(MonoDataset):
+class KITTIDatasetBACK(MonoDataset):
     """Superclass for different types of KITTI dataset loaders
     """
     def __init__(self, *args, **kwargs):
-        super(KITTIDataset, self).__init__(*args, **kwargs)
+        super(KITTIDatasetBACK, self).__init__(*args, **kwargs)
 
         # NOTE: Make sure your intrinsics matrix is *normalized* by the original image size.
         # To normalize you need to scale the first row by 1 / image_width and the second row
@@ -62,48 +33,49 @@ class KITTIDataset(MonoDataset):
 # K是相机内参 stereo_T是相机外参，也就是转换矩阵,4行4列，16维度
         self.full_res_shape = (1242, 375)
         self.side_map = {"2": 2, "3": 3, "l": 2, "r": 3}
-        self.gcs = GCSHandler(self.data_path)
 
     def check_depth(self):
         line = self.filenames[0].split()
         scene_name = line[0]
         frame_index = int(line[1])
 
-        velo_rel_path = os.path.join(
+        velo_filename = os.path.join(
+            self.data_path,
             scene_name,
             "velodyne_points/data/{:010d}.bin".format(int(frame_index)))
 
-        if self.gcs.is_gcs:
-            gcs_full_path = os.path.join(self.gcs.data_path, velo_rel_path)
-            return self.gcs.fs.exists(gcs_full_path)
-        else:
-            local_path = os.path.join(self.data_path, velo_rel_path)
-            return os.path.isfile(local_path)
+        return os.path.isfile(velo_filename)
 
     def get_color(self, folder, frame_index, side, do_flip):
-        path = self.get_image_path(folder, frame_index, side)
-        color = self.gcs.open_image(path)
+        color = self.loader(self.get_image_path(folder, frame_index, side))
+
         if do_flip:
             color = color.transpose(pil.FLIP_LEFT_RIGHT)
+
         return color
 
 
-class KITTIRAWDataset(KITTIDataset):
+class KITTIRAWDatasetBACK(KITTIDataset):
     """KITTI dataset which loads the original velodyne depth maps for ground truth
     """
     def __init__(self, *args, **kwargs):
-        super(KITTIRAWDataset, self).__init__(*args, **kwargs)
+        super(KITTIRAWDatasetBACK, self).__init__(*args, **kwargs)
 
     def get_image_path(self, folder, frame_index, side):
         f_str = "{:010d}{}".format(frame_index, self.img_ext)
-        return self.gcs.get_file_path(folder, "image_0{}/data".format(self.side_map[side]), f_str)
+        image_path = os.path.join(
+            self.data_path, folder, "image_0{}/data".format(self.side_map[side]), f_str)
+        return image_path
 
     def get_depth(self, folder, frame_index, side, do_flip):
-        calib_path = self.gcs.get_file_path(folder.split("/")[0])
+        calib_path = os.path.join(self.data_path, folder.split("/")[0])
 
-        velo_path = self.gcs.get_file_path(folder, "velodyne_points/data", "{:010d}.bin".format(frame_index))
+        velo_filename = os.path.join(
+            self.data_path,
+            folder,
+            "velodyne_points/data/{:010d}.bin".format(int(frame_index)))
 
-        depth_gt = generate_depth_map(calib_path, velo_path, self.side_map[side], use_gcs=True, gcs=self.gcs.fs, gcs_root=self.gcs.data_path)
+        depth_gt = generate_depth_map(calib_path, velo_filename, self.side_map[side])
         depth_gt = skimage.transform.resize(
             depth_gt, self.full_res_shape[::-1], order=0, preserve_range=True, mode='constant')
 
@@ -113,32 +85,46 @@ class KITTIRAWDataset(KITTIDataset):
         return depth_gt
 
 
-class KITTIOdomDataset(KITTIDataset):
+class KITTIOdomDatasetBACK(KITTIDataset):
     """KITTI dataset for odometry training and testing
     """
     def __init__(self, *args, **kwargs):
-        super(KITTIOdomDataset, self).__init__(*args, **kwargs)
+        super(KITTIOdomDatasetBACK, self).__init__(*args, **kwargs)
 
     def get_image_path(self, folder, frame_index, side):
         f_str = "{:06d}{}".format(frame_index, self.img_ext)
-        return self.gcs.get_file_path("sequences/{:02d}".format(int(folder)), "image_{}".format(self.side_map[side]), f_str)
+        image_path = os.path.join(
+            self.data_path,
+            "sequences/{:02d}".format(int(folder)),
+            "image_{}".format(self.side_map[side]),
+            f_str)
+        return image_path
 
 
-class KITTIDepthDataset(KITTIDataset):
+class KITTIDepthDatasetBACK(KITTIDataset):
     """KITTI dataset which uses the updated ground truth depth maps
     """
     def __init__(self, *args, **kwargs):
-        super(KITTIDepthDataset, self).__init__(*args, **kwargs)
+        super(KITTIDepthDatasetBACK, self).__init__(*args, **kwargs)
 
     def get_image_path(self, folder, frame_index, side):
         f_str = "{:010d}{}".format(frame_index, self.img_ext)
-        return self.gcs.get_file_path(folder, "image_0{}/data".format(self.side_map[side]), f_str)
+        image_path = os.path.join(
+            self.data_path,
+            folder,
+            "image_0{}/data".format(self.side_map[side]),
+            f_str)
+        return image_path
 
     def get_depth(self, folder, frame_index, side, do_flip):
         f_str = "{:010d}.png".format(frame_index)
-        depth_path = self.gcs.get_file_path(folder, "proj_depth/groundtruth/image_0{}".format(self.side_map[side]), f_str)
+        depth_path = os.path.join(
+            self.data_path,
+            folder,
+            "proj_depth/groundtruth/image_0{}".format(self.side_map[side]),
+            f_str)
 
-        depth_gt = self.gcs.open_depth(depth_path)
+        depth_gt = pil.open(depth_path)
         depth_gt = depth_gt.resize(self.full_res_shape, pil.NEAREST)
         depth_gt = np.array(depth_gt).astype(np.float32) / 256
 
