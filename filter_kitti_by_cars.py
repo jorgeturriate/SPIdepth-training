de557fff6b8345be160451d89dfd8a04b27c88a0
jorgeturriate <jorge_xt@hotmail.com>
2025-05-11 16:11:36 +0200

    script for generating a reduced train set

--- filter_kitti_by_cars.py
+++ filter_kitti_by_cars.py
@@ -80,11 +80,11 @@ def main(input_txt, output_txt, box_json, remaining_txt, data_path, num_images=1
 if __name__ == '__main__':
     # Example call in Colab:
     main(
-        #input_txt="/content/SPIdepth-training/splits/eigen_zhou/train_files_original.txt",
-        input_txt="/content/train_files_remaining.txt",
-        output_txt="/content/filtered_train_files6.txt",
-        box_json="/content/car_boxes6.json",
-        remaining_txt="/content/train_files_remaining2.txt",
+        input_txt="/content/SPIdepth-training/splits/eigen_zhou/train_files_original.txt",
+        #input_txt="/content/train_files_remaining.txt",
+        output_txt="/content/filtered_val_files1.txt",
+        box_json="/content/carval_boxes1.json",
+        remaining_txt="/content/val_files_remaining.txt",
         data_path="gs://mde_data_bucket/kitti",
-        num_images=400
+        num_images=200
     )
