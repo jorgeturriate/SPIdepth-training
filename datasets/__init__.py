10b7e79ab0d05705d2ea93c1b61a28aa58ac1a53
jorgeturriate <jorge_xt@hotmail.com>
2025-06-13 22:56:47 +0200

    Added compatibility with Midair dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -6,3 +6,4 @@ from .mono_dataset import *
 from .mono_dataset_city import *
 from .cityscapes_preprocessed_dataset import CityscapesPreprocessedDataset
 from .cityscapes_evaldataset import CityscapesEvalDataset
+from .midair_dataset import MidAirDataset
